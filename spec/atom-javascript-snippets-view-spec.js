2eeffd0a9a739bc923fcb2bf94f22bc63b1f4993
johang2012 <johan.gudmundsson2012@gmail.com>
2017-06-04 03:48:22 +0200

    init

---
+++ spec/atom-javascript-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomJavascriptSnippetsView from '../lib/atom-javascript-snippets-view';
+
+describe('AtomJavascriptSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
